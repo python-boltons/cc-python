546dbea69c1c20e357bc85430489b0ebaeaefda3
Bryan Bugyi <bryanbugyi34@gmail.com>
2021-09-14 11:21:19 -0400

    Add typeguard check

--- {{ cookiecutter.package_name }}/tests/conftest.py
+++ {{ cookiecutter.package_name }}/tests/conftest.py
@@ -2,3 +2,20 @@
 
 https://docs.pytest.org/en/6.2.x/fixture.html#conftest-py-sharing-fixtures-across-multiple-files
 """
+
+from typing import Any
+
+from typeguard.importhook import install_import_hook
+
+
+def pytest_configure(config: Any) -> None:
+    """Setup typeguard importhooks.
+
+    NOTE: We cannot use --typeguard-packages=tests since we currently get the
+    following error:
+        typeguard cannot check these packages because they are already imported
+    """
+    del config
+
+    install_import_hook("tests")
+    install_import_hook("{{ cookiecutter.package_name }}")
