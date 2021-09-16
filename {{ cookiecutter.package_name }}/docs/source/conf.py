328135d36661c4b1c1a476f60abdf35d0cdf1547
Bryan Bugyi <bryanbugyi34@gmail.com>
2021-09-16 10:52:40 -0400

    Add docs/ directory

---
+++ {{ cookiecutter.package_name }}/docs/source/conf.py
@@ -0,0 +1,69 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../../src/"))
+
+import sphinx_theme  # noqa
+
+# -- Project information -----------------------------------------------------
+
+project = "{{ cookiecutter.package_name }}"
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx_autodoc_typehints",
+    "recommonmark",
+]
+source_suffix = {".rst": "restructuredtext", ".md": "markdown"}
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
+
+napoleon_google_docstring = True
+napoleon_use_admonition_for_examples = True
+# We are using the typehints extension instead of the
+# default autodoc type hints as they allow disabling
+# fully qualified names which can get very ugly for
+# type hints.
+autodoc_typehints = "none"
+always_document_param_types = True
+add_module_names = False
+typehints_fully_qualified = False
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+
+# -- Sphinx themes -----------------------------------------------------------
+html_theme = "neo_rtd_theme"
+html_theme_path = [sphinx_theme.get_html_theme_path()]
---
+++ {{ cookiecutter.package_name }}/docs/source/example.md
@@ -0,0 +1,18 @@
+# Example Markdown
+
+## Features
+You can use all the markdown features here
+
+ * Like lists
+ * More lists
+
+It also supports language specific formatting
+
+```bash
+make test
+```
+
+```python
+test = "awesome"
+"".join(test.split(""))
+```
