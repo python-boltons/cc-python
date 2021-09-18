5722761e5730f710a6d092525716f91745543992
Bryan Bugyi <bryanbugyi34@gmail.com>
2021-09-17 21:47:39 -0400

    Fix pre_gen_project.py hook issues

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,24 +1,19 @@
-"""Cookiecutter pre-hooks.."""
+"""Cookiecutter pre-hooks.
+
+### Set extra cookiecutter variables.
+{{
+    cookiecutter.update({
+        'package_path': cookiecutter.package_name|replace('.', '/')
+    })
+}}
+"""
 
 import json
-import os
-from typing import MutableMapping
 
 
 def main() -> None:
     context = json.loads("""{{ cookiecutter | jsonify }}""")
-    set_package_dir(context, context["package_name"])
-
-
-def set_package_dir(
-    context: MutableMapping[str, str], package_name: str
-) -> None:
-    package_path = package_name.replace(".", os.sep)
-    context["package_path"] = package_path
-    print(
-        "Set 'package_path' cookiecutter variable:"
-        f" [{{{{ cookiecutter.update({context}) }}}}]"
-    )
+    print(f"context = {context}")
 
 
 if __name__ == "__main__":
