8c5791aac7f4432dc68a7a9cbeb6b5f35a124b05
Bryan Bugyi <bryanbugyi34@gmail.com>
2021-09-17 21:07:21 -0400

    Normalize namespace package directories

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,12 +1,21 @@
 import json
 import os
 from pathlib import Path
+import shutil
 import subprocess
 from subprocess import PIPE
 
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 GITHUB_URL = "git@github.com:{{ cookiecutter.git_org_name }}/{}.git".format
+
+
+def normalize_namespace_path(package_namespace: str) -> None:
+    if "." in package_namespace:
+        module_dir = package_namespace.replace(".", os.sep)
+        old_dir = package_namespace
+        new_dir = module_dir
+        shutil.move(old_dir, new_dir)
 
 
 def create_git_repo(package_name: str) -> None:
@@ -31,6 +40,7 @@ def create_git_repo(package_name: str) -> None:
 def main() -> None:
     context = json.loads("""{{ cookiecutter | jsonify }}""")
     create_git_repo(context["git_repo_name"])
+    normalize_namespace_path(context["package_namespace"])
 
 
 if __name__ == "__main__":
