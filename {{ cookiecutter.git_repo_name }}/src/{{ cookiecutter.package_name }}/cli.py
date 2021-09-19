3f77743ab0e9cd0bb2b5e7e3a20b1c2f8adb6cc7
Bryan Bugyi <bryanbugyi34@gmail.com>
2021-09-19 14:58:01 -0400

    Boost initial test coverage to 100%

--- {{ cookiecutter.git_repo_name }}/src/{{ cookiecutter.package_name }}/cli.py
+++ {{ cookiecutter.git_repo_name }}/src/{{ cookiecutter.package_name }}/cli.py
@@ -6,7 +6,7 @@ from typing import Sequence
 
 def main(argv: Sequence[str] = None) -> int:
     """Main entry point."""
-    if argv is None:
+    if argv is None:  # pragma: no cover
         argv = sys.argv
 
     return 0
