08292a8fef0689d6fdfb3a851222ada7458c2dab
Bryan Bugyi <bryanbugyi34@gmail.com>
2021-09-20 10:09:27 -0400

    Use 'Config' instead of 'Any'

--- {{ cookiecutter.git_repo_name }}/tests/conftest.py
+++ {{ cookiecutter.git_repo_name }}/tests/conftest.py
@@ -3,12 +3,11 @@
 https://docs.pytest.org/en/6.2.x/fixture.html#conftest-py-sharing-fixtures-across-multiple-files
 """
 
-from typing import Any
-
+from _pytest.config import Config
 from typeguard.importhook import install_import_hook
 
 
-def pytest_configure(config: Any) -> None:
+def pytest_configure(config: Config) -> None:
     """Setup typeguard importhooks.
 
     NOTE: We cannot use --typeguard-packages=tests since we currently get the
