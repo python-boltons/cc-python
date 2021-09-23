7ca0cac57d0da8ceaec71105e553c4585ebbb0d1
Bryan Bugyi <bryanbugyi34@gmail.com>
2021-09-23 14:56:33 -0400

    Improve conftest.py docstring

--- {{cookiecutter.git_repo_name}}/tests/conftest.py
+++ {{cookiecutter.git_repo_name}}/tests/conftest.py
@@ -1,4 +1,4 @@
-"""Shared fixture file used by pytest.
+"""This file contains shared fixtures and pytest hooks.
 
 https://docs.pytest.org/en/6.2.x/fixture.html#conftest-py-sharing-fixtures-across-multiple-files
 """
