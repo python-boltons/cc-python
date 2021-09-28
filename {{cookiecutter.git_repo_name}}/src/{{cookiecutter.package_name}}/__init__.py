10d095485e6278774f2e7475a6315ddf811c2726
Bryan Bugyi <bryanbugyi34@gmail.com>
2021-09-28 12:18:18 -0400

    Fix whitespace

--- {{cookiecutter.git_repo_name}}/src/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.git_repo_name}}/src/{{cookiecutter.package_name}}/__init__.py
@@ -1,9 +1,9 @@
 """{{ cookiecutter.package_description }}"""
+{%- if cookiecutter.package_type == "library" %}
+
+from .{{ cookiecutter.package_module }} import dummy
+{% endif %}
 
 __author__ = "{{ cookiecutter.author }}"
 __email__ = "{{ cookiecutter.email }}"
 __version__ = "0.0.1"
-
-{% if cookiecutter.package_type == "library" -%}
-from .{{ cookiecutter.package_module }} import dummy
-{%- endif %}
