8be15cc5ee4b11356548f42176b2ec4cebce8048
Bryan Bugyi <bryanbugyi34@gmail.com>
2021-09-28 21:38:06 -0400

    Add NullHandler in __init__.py

--- {{cookiecutter.git_repo_name}}/src/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.git_repo_name}}/src/{{cookiecutter.package_name}}/__init__.py
@@ -1,9 +1,15 @@
 """{{ cookiecutter.package_description }}"""
 {%- if cookiecutter.package_type == "library" %}
 
+import logging as _logging
+
 from .{{ cookiecutter.package_module }} import dummy
 {% endif %}
 
 __author__ = "{{ cookiecutter.author }}"
 __email__ = "{{ cookiecutter.email }}"
 __version__ = "0.0.1"
+{%- if cookiecutter.package_type == "library" %}
+
+_logging.getLogger(__name__).addHandler(_logging.NullHandler())
+{%- endif %}
