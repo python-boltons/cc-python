2652c37e485655848bffab7c37aea462d9eb47d0
Bryan Bugyi <bryanbugyi34@gmail.com>
2022-01-03 12:56:50 -0500

    Revert "Add hack to fix pytest logging bug"
    This reverts commit 3e3155c1e324fda58ce20153c8d815cec2fcf2f2.

--- {{cookiecutter.git_repo_name}}/tests/conftest.py
+++ {{cookiecutter.git_repo_name}}/tests/conftest.py
@@ -2,26 +2,3 @@
 
 https://docs.pytest.org/en/6.2.x/fixture.html#conftest-py-sharing-fixtures-across-multiple-files
 """
-
-import logging
-
-from pytest import fixture
-import structlog
-
-
-@fixture(autouse=True)
-def clear_loggers() -> None:
-    """Remove handlers from all loggers and unconfigure structlog.
-
-    See https://github.com/pytest-dev/pytest/issues/5502 for an explanation on
-    why we need this fixture.
-    """
-    loggers = [logging.getLogger()] + list(
-        logging.Logger.manager.loggerDict.values()  # type: ignore[arg-type]
-    )
-    for logger in loggers:
-        handlers = getattr(logger, "handlers", [])
-        for handler in handlers:
-            logger.removeHandler(handler)
-
-    structlog.reset_defaults()
